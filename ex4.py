7ea91c354d486bc206170e254c8763bfe8b08781
Rhys <rhysrichmond1@gmail.com>
2018-10-03 17:56:32 +0100

    Fixed spelling issues

--- ex4.py
+++ ex4.py
@@ -1,27 +1,27 @@
-my_name = 'Rhys Richmond'
-my_age = 18
-my_height = 69 #Inches / 175 cm
-my_weight = 155 #Pounds
-my_eyes = 'Grey'
-my_hair = 'Brown/Blonde'
+myName = 'Rhys Richmond'
+myAge = 18
+myHeight = 69 #Inches
+myWeight = 155 #Pounds
+myEyes = 'Grey'
+myHair = 'Brown/Blonde'
+# is_heavy works by giving a boolean based on if the number is greater or lower then 3000 .
 is_heavy = my_weight > 3000
 
+# I have added variables to calculate the conversions and then used round to make the numbers to 2 decimal places so it looks neat.
 WeightConversion = my_weight / 2.20
 HeightConversion = my_height * 2.54
 
 NewWeight = round(WeightConversion, 2)
 NewHeight = round(HeightConversion, 2)
 
-print(f"Let's talk about {my_name}.")
-print(f"He is {my_height} inches tall or {NewHeight} cm tall.")
-print(f"He's {my_weight} pounds heavy or {NewWeight} kg heavy.")
+print(f"Let's talk about {myName}.")
+print(f"He is {myHeight} inches tall or {NewHeight} cm tall.")
+print(f"He's {myWeight} pounds heavy or {NewWeight} kg heavy.")
 print(f"It is {is_heavy} that he is overweight")
-print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"He's got {myEyes} eyes and {myHair} hair.")
 
-total = my_age + my_height + my_weight
-print(f"If I add {my_age}, {my_height}, {my_weight} I get {total}")
+total = myAge + myHeight + myWeight
+print(f"If I add {myAge}, {myHeight}, {myWeight} I get {total}")
 
 
-
-# is_heavy works by giving a boolean based on if the number is greater then 3000.
-#Renaming the variables requires changing it in the print sections too.
+# I have changed the variable names to my prefered way of typing variables which is camel case.
