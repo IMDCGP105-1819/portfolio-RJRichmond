de5807340f672a1b56faf59ba1089bcbc452f3ae
Rhys <rhysrichmond1@gmail.com>
2018-10-03 17:54:15 +0100

    Fixed Grammer issues

--- ex1.py
+++ ex1.py
@@ -3,8 +3,8 @@ print ('This is exciting.')
 print ("Isn't this a thrill?")
 print ("I said isn't this a thrill?")
 print ('No, you are right, it is "boring".')
+print ("Hi there")
 
-#Python
-#comments don't show in cmd
-# mixing quoations then can add everything to the print
-# when putting on one line you get a syntax error
+# Putting a "#" in front of a line comments it.
+# mixing different quotations means the quote is still open so it will carry on until it is closed.
+# if you move everything to one line it will give you a syntax error.
