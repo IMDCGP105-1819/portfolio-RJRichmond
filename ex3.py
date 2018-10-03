d595463785af60fe840d95a32d85f5f8148006a6
Rhys <rhysrichmond1@gmail.com>
2018-10-03 17:55:40 +0100

    Changed Commenting

--- ex3.py
+++ ex3.py
@@ -1,18 +1,14 @@
-#
+# Here the variables are being created and are assigned values.
 cars = 100
-#
 space_in_a_car = 4.0
-#
 drivers = 30
-#
 passengers = 90
-#
+
+# Then some new variables are being created using the previously created variables, for example "cars_not_driven" is taking the
+# cars variable and subtracting it from the drivers variable.
 cars_not_driven = cars - drivers
-#
 cars_driven = drivers
-#
 carpool_capacity = cars_driven * space_in_a_car
-#
 average_passengers_per_car = passengers / cars_driven
 
 print ("There are", cars, "cars available.")
@@ -23,4 +19,4 @@ print ("We have", passengers, "to carpool today.")
 print ("We need to put about", average_passengers_per_car, "in each car.")
 
 #The error is due to the variable name not being inputted properly.
-#The difference is using a float and int so it won't print it as a decimal number.
+#The difference between using a float and a interger is that a float will print as a decimal and a interger won't.
