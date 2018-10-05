9979182344ed79f20770e35bf84329ad86c7711a
Rhys <rhysrichmond1@gmail.com>
2018-10-05 13:20:46 +0100

    First version of ex5

---
+++ ex5.py
@@ -0,0 +1,9 @@
+bottlesOfBeer = 99
+
+for i in range(1,100,1):
+    print (bottlesOfBeer, "bottles of beer on the wall,", bottlesOfBeer, "bottles of beer.")
+    bottlesOfBeer = bottlesOfBeer - 1
+    print ("Take one down, pass it around", bottlesOfBeer, "bottles of beer on the wall...")
+
+print ("No more bottles of beer on the wall, no more bottles of beer.")
+print ("Go to the store buy some more, 99 bottles of beer on the wall...")
