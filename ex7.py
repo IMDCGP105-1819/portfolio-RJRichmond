f01cf1d8b3284664536d8c1bff90bd1fc6b25e79
Rhys <rhysrichmond1@gmail.com>
2018-10-05 14:31:26 +0100

    Working version with corrected formatting

--- ex7.py
+++ ex7.py
@@ -6,17 +6,21 @@ annual_salary = 0.0
 r = 0.04 #r is annual return percentage 4%
 portion_saved = 0.0
 
-annual_salary = float(input("Please input your annual salary"))
-portion_saved = float(input("Please input the percentage of money you will save per month"))
-total_cost = float(input("Please input the cost of your dream home"))
+annual_salary = float(input("Enter your annual salary: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+total_cost = float(input("Enter the cost of your dream home: "))
 
-# This section takes the values the user inputted and then uses them to work out the monthly salary, how much they are saving in total per month and what there return on the interest is.
+# This section takes the values the user inputted and then uses them to work out the monthly salary, deposit amount, total saved per month and what their return on the interest is.
 monthly_salary = (annual_salary / 12)
+depositAmount = (total_cost * portion_deposit)
+print (depositAmount)
 current_savings = (monthly_salary * portion_saved)
-monthlyReturn = (current_savings * r) / 12
+monthlyReturn = (current_savings * r/12)
+
 
 #This then works out the total amount being saved a month and the number of months needed to reach the goal.
 totalPerMonth = (current_savings + monthlyReturn)
-numberOfMonthsNeeded = (total_cost / totalPerMonth)
+print (totalPerMonth)
+numberOfMonthsNeeded = (depositAmount / totalPerMonth)
 
-print ("Number of months needed:",numberOfMonthsNeeded)
+print ("Number of months: ",numberOfMonthsNeeded)
