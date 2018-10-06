2b9ce201a3fada09e94e01dc26d1ad7b23ed17c9
Rhys <rhysrichmond1@gmail.com>
2018-10-06 19:21:03 +0100

    Working version of the calculator with comments

--- ex7.py
+++ ex7.py
@@ -2,6 +2,8 @@ total_cost = 0.0
 portion_deposit = 0.20 # 20% needed for a deposit
 current_savings = 0.0
 annual_salary = 0.0
+numberOfMonthsNeeded = 0.0
+
 
 r = 0.04 #r is annual return percentage 4%
 portion_saved = 0.0
@@ -13,14 +15,17 @@ total_cost = float(input("Enter the cost of your dream home: "))
 # This section takes the values the user inputted and then uses them to work out the monthly salary, deposit amount, total saved per month and what their return on the interest is.
 monthly_salary = (annual_salary / 12)
 depositAmount = (total_cost * portion_deposit)
-print (depositAmount)
-current_savings = (monthly_salary * portion_saved)
-monthlyReturn = (current_savings * r/12)
 
 
-#This then works out the total amount being saved a month and the number of months needed to reach the goal.
-totalPerMonth = (current_savings + monthlyReturn)
-print (totalPerMonth)
-numberOfMonthsNeeded = (depositAmount / totalPerMonth)
+
+#This loop runs when the savings is less then the deposit, the loop adds the monthly savings, the interest and then it adds 1 to the month variable, until the amount is met.
+
+while current_savings < depositAmount :
+    current_savings = current_savings + (monthly_salary * portion_saved)
+    current_savings = current_savings + current_savings*(r/12)
+
+    numberOfMonthsNeeded = numberOfMonthsNeeded + 1
+
+
 
 print ("Number of months: ",numberOfMonthsNeeded)
