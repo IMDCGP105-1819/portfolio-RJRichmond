97c92b8791c8799892c1b08371842f23a884e3b7
Rhys <rhysrichmond1@gmail.com>
2018-10-24 12:27:43 +0100

    Stopped using global variables and starting using variables to store the return instead. It now give correct results

--- ex12.py
+++ ex12.py
@@ -1,56 +1,45 @@
 def Hotel_cost(nights):
-    #nights = int(input("How many nights are you staying?"))
-    #HotelTotalCost = nights * 70
-    nights = nights * 70
+    return nights * 70
 
 def plane_ticket(city, PlaneClass):
-    #print ("Destinations you can travel to : New york, Auckland, Venice and Glasgow.")
-    #print ("Make sure to input exactly as they appear!")
-    #city = int(input("What city are you traveling to?"))
-
-    #print ("Avaliable classes: Economy = 1, Premium Economy = 1.3, Business Class = 1.6 and First Class = 1.9")
-    #PlaneClass = float(input("Please enter the multiplier of the class you will be flying"))
     if (city == "New york"):
         TotalPlaneCost = 2000 * PlaneClass
         PlaneClass = TotalPlaneCost
+        return PlaneClass
+
     elif (city == "Auckland"):
         TotalPlaneCost = 790 * PlaneClass
         PlaneClass = TotalPlaneCost
+        return PlaneClass
     elif (city == "Venice"):
         TotalPlaneCost = 154 * PlaneClass
-        PlaneClass = TotalPlaneCost
+        PlaneClass == TotalPlaneCost
+        return PlaneClass
     elif (city == "Glasgow"):
         TotalPlaneCost = 65 * PlaneClass
-        PlaneClass = TotalPlaneCost
+        PlaneClass == TotalPlaneCost
+        return PlaneClass
     else:
         return
 
 def Rental_car(days):
-    #days = int(input("How many days are you going to be their"))
-
     totalRentalCarCost = days * 30
-
     if (days > 7):
         days = totalRentalCarCost -50
+        return days
     elif ( 7 > days >= 3):
         days = totalRentalCarCost -30
+        return days
 
-def total_cost (city, days, nights):
-    #Hotel_cost();
-    #plane_ticket();
-    #Rental_car();
-    total_cost = nights + PlaneClass + days
-    print ("It would cost", total_cost)
+def total_cost(int):
+    nights = Hotel_cost(int(input("How many nights are you staying? ")))
+    city = plane_ticket(city = str(input("What city are you traveling to? ")),PlaneClass = float(input("Please enter the multiplier of the class you will be flying ")) )
+    days = Rental_car(days = int(input("How many days are you going to be there ")))
+    total_cost = nights + city + days
+    return total_cost
 
-nights = int(input("How many nights are you staying? "))
-Hotel_cost(nights);
 print ("Destinations you can travel to : New york, Auckland, Venice and Glasgow. ")
 print ("Make sure to input exactly as they appear! ")
-city = str(input("What city are you traveling to? "))
-
 print ("Avaliable classes: Economy = 1, Premium Economy = 1.3, Business Class = 1.6 and First Class = 1.9 ")
-PlaneClass = float(input("Please enter the multiplier of the class you will be flying "))
-plane_ticket(city, PlaneClass);
-days = int(input("How many days are you going to be there "))
-Rental_car(days);
-total_cost(city, days, nights);
+price = total_cost(int)
+print ("It will cost you", price)
